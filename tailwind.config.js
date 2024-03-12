83a398685d5c2a70f45f7643b6941b779befe8ff
Gabriela Fernandes <gabriela.cavalcanti886@gmail.com>
2024-03-12 16:46:26 -0300

    n aguento mais

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,9 @@ module.exports = {
     extend: {
       colors: {
         "blue": "#4D6688",
-        "maroon": "#BB0000"
+        "maroon": "#BB0000",
+        "gray": "#130000",
+        "grayclarinho": "#6D6D6D"
       },
       fontFamily:{
         font: ["Irish Grover", "system-ui"],
